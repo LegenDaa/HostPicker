769fdecce5f07197dbe78b349235ab6149199518
wb-daixiulei <daixiulei@163.com>
2018-01-04 18:36:12 +0800

    修改目录结构

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function createWindow () {
   mainWindow.webContents.openDevTools ()
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, './src/index.html'),
     protocol: 'file:',
     slashes: true
   }))
